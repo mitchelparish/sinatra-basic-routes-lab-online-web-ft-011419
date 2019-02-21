4d263d2927bcfb8a9728b5b98be88801604aff6a
Mitchel Parish <mitchelparish@gmail.com>
2019-02-21 21:06:32 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Mitch."
+  end
+
+  get '/hometown' do
+    "My hometown is Whitesboro, Texas."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Johnny B. Goode. "
+  end
+
 end
